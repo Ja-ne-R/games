9fcbe6d9fc004eb5418495773eb8e2c6a1af80d4
Jane <jane.ronk@gmail.com>
2025-09-09 10:26:00 +0300

    work

---
+++ games.js
@@ -0,0 +1,43 @@
+fetch('https://ja-ne-r.github.io/games/games.json')
+    .then(response => response.json())
+    .then(data => {
+        const games = data.games;
+        const gamemain = document.querySelector('#gamesmain');
+        games.forEach(game => {
+            const gameDiv = document.createElement('div');
+            gameDiv.classList.add('game');
+            gameDiv.innerHTML = `
+                <img src="${game.image}" class="imp" alt="${game.name}">
+                <h3 class="gamename">${game.name}</h3>
+                <p class="gametext">${game.description}</p>
+            `;
+            gamemain.appendChild(gameDiv);
+        });
+        const img = document.getElementsByClassName("imp");
+
+        const imgnum = img.length;
+        for (let i = 0; i < imgnum; i++) {
+            img[i].addEventListener("mouseenter", function () {
+            });
+            img[i].addEventListener("mouseleave", function () {
+            });
+        }
+
+        const gamediv = document.getElementsByClassName("game");
+
+        const gamedivnum = gamediv.length;
+        for (let i = 0; i < gamedivnum; i++) {
+            gamediv[i].addEventListener("mouseenter", function () {
+                gamediv[i].style.animation = "gamehover 0.5s forwards";
+                img[i].style.animation = "test 1s forwards";
+
+            });
+            gamediv[i].addEventListener("mouseleave", function () {
+                gamediv[i].style.animation = "gamehoverout 0.5s forwards";
+                img[i].style.animation = "out 1s forwards";
+
+            });
+        }
+
+    })
+    .catch(error => console.error('Error loading games:', error));
--- javascript.js
+++ javascript.js
@@ -1,32 +1,22 @@
-const rock = document.querySelector("#rock img");
+const rock = document.getElementById("rock");
 const aside = document.getElementById("featured");
-const  img = document.querySelector("body img");
 
 
-img.addEventListener("mouseenter", function () {
-    img.style.animation = "test 1s forwards";
-});
 
-img.addEventListener("mouseleave", function () {
-    img.style.animation = "out 1s forwards";
-});
+
 
 
 // ----------------------------
-fetch('https://ja-ne-r.github.io/games/games.json')
-    .then(response => response.json())
-    .then(data => {
-        const games = data.games;
-        const gamemain = document.querySelector('#gamesmain');
-        games.forEach(game => {
-            const gameDiv = document.createElement('div');
-            gameDiv.classList.add('game');
-            gameDiv.innerHTML = `
-                <img src="${game.image}" class="gameimg" alt="${game.name}">
-                <h3 class="gamename">${game.name}</h3>
-                <p class="gametext">${game.description}</p>
-            `;
-            gamemain.appendChild(gameDiv);
-        });
-    })
-    .catch(error => console.error('Error loading games:', error));
+
+
+
+    // ------------------------------------
+
+
+rock.addEventListener("mouseenter", function () {
+   rock.style.animation = "test 1s forwards";
+});
+
+rock.addEventListener("mouseleave", function () {
+    rock.style.animation = "out 1s forwards";
+});
