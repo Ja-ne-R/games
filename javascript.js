67e5efd72a4e3ac26c662f6b2b0c949110d150dd
Jane <jane.ronk@gmail.com>
2025-09-10 12:29:04 +0300

    upgrade

--- javascript.js
+++ javascript.js
@@ -13,13 +13,13 @@ const aside = document.getElementById("featured");
     // ------------------------------------
 
 
-rock.addEventListener("mouseenter", function () {
-   rock.style.animation = "test 1s forwards";
-});
+// rock.addEventListener("mouseenter", function () {
+//    rock.style.animation = "test 1s forwards";
+// });
 
-rock.addEventListener("mouseleave", function () {
-    rock.style.animation = "out 1s forwards";
-});
+// rock.addEventListener("mouseleave", function () {
+//     rock.style.animation = "out 1s forwards";
+// });
 
 
 
--- projects.js
+++ projects.js
@@ -2,18 +2,53 @@ fetch('https://ja-ne-r.github.io/games/games.json')
     .then(response => response.json())
     .then(data => {
         const projects = data.projects;
-        const main = document.querySelector('main');
-        projects.forEach(projects => {
+        const main = document.querySelector('#projects');
+        projects.forEach(project => {
             const projDiv = document.createElement('div');
             projDiv.classList.add('projects');
-            projDiv.innerHTML = `
-                <img src="${projects.image}" class="imp" alt="${projects.name}">
-                <h3 class="projectname">${projects.name}</h3>
-                <p class="projecttext">${projects.description}</p>
-            `;
+
+            const img = document.createElement('img');
+            img.src = project.image;
+            img.className = 'proimp';
+            img.alt = project.name;
+
+
+            img.addEventListener('click', function() {
+                console.log('Image clicked!');
+                const modal = document.getElementById("modal");
+                const modalImg = modal.querySelector(".modal-content");
+                const caption = modal.querySelector("#caption");
+                caption.innerHTML = "<b>" + project.name + "</b>" + "<br>" + project.description;
+                modal.style.display = "block";
+                modalImg.src = img.src;
+
+                const span = modal.querySelector(".close");
+                span.onclick = function() { 
+                    modal.style.display = "none";
+                }
+
+            });
+            
+            const h3 = document.createElement('h3');
+            h3.className = 'projectname';
+            h3.textContent = project.name;
+
+            const p = document.createElement('p');
+            p.className = 'projecttext';
+            p.textContent = project.description;
+
+            projDiv.appendChild(img);
+            projDiv.appendChild(h3);
+            projDiv.appendChild(p);
+
             main.appendChild(projDiv);
         });
-        const img = document.getElementsByClassName("imp");
 
     })
     .catch(error => console.error('Error loading projects:', error));
+
+
+    // -------------------------
+    
+
+    
